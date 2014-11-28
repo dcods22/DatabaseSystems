e317ea85a2005d33dab7c3ab121dbb0215c5dae7
Dcods22 <Daniel.cody2@marist.edu>
2014-11-28 14:37:40 -0500

    Updated Design Project

--- Assignment 10/procedures.sql
+++ Assignment 10/procedures.sql
@@ -11,3 +11,10 @@ table(prereqnum integer) as $$
 	SELECT prereqnum FROM prerequisites WHERE prereqnum = wantedPreReqNum
 
 $$ language 'sql';
+
+CREATE OR REPLACE FUNCTION MostUsedForum() RETURNS 
+TABLE (forumName text, count integer) as $$
+
+	SELECT forumID, max(num) FROM (SELECT forumID, COUNT(forumID) as num FROM subForums GROUP BY forumID) subForums GROUP BY forumID ORDER BY MAX DESC LIMIT 1;
+	
+$$ language 'sql' $$
